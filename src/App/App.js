ed7b983a502d8eb4bb4bfde0610bf8e1dbeb5461
Chase Hamby <c.chase.hamby@gmail.com>
2018-12-12 19:56:59 -0600

    authed state working

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 import connection from '../helpers/data/connection';
 import Auth from '../components/Auth/auth';
+import Profile from '../components/Profile/profile';
 import './App.scss';
 
 class App extends Component {
+  state = {
+    authed: false,
+  }
+
   componentDidMount() {
     connection();
   }
 
+  isAuthenticated = () => {
+    this.setState({ authed: true });
+  }
+
   render() {
+    if (!this.state.authed) {
+      return (
+        <div className="App">
+          <Auth isAuthenticated={this.isAuthenticated}/>
+        </div>
+      );
+    }
     return (
       <div className="App">
-        <Auth />
+        <Profile />
       </div>
     );
   }
