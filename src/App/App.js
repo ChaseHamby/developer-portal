0d974eb265f4f90238c75ef5f32018ffbe54e07b
Chase Hamby <c.chase.hamby@gmail.com>
2018-12-12 20:13:51 -0600

    auth done

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import connection from '../helpers/data/connection';
 import Auth from '../components/Auth/auth';
 import Profile from '../components/Profile/profile';
@@ -13,6 +15,21 @@ class App extends Component {
 
   componentDidMount() {
     connection();
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+        });
+      } else {
+        this.setState({
+          authed: false,
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
   }
 
   isAuthenticated = () => {
